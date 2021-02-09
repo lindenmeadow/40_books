7a8f402c015918b81eedded963d8aef8c2682f14
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2021-02-09 17:36:08 -0500

    Updating forms, enabled enumeration of books in book index, updated stylesheet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,34 @@ class HomeController < ApplicationController
   end
 
   def books
+    @award_winner_books = AwardWinnerBook.all
+    @awcount = @award_winner_books.count
+
+    @biography_books = BiographyBook.all
+    @bcount = @biography_books.count
+
+    @fantasy_books = FantasyBook.all
+    @fcount = @fantasy_books.count
+
+    @historical_fiction_books = HistoricalFictionBook.all
+    @hfcount = @historical_fiction_books.count
+
+    @informational_books = InformationalBook.all
+    @icount = @informational_books.count
+
+    @mystery_books = MysteryBook.all
+    @mcount = @mystery_books.count
+
+    @poetry_anthology_books = PoetryAnthologyBook.all
+    @pacount = @poetry_anthology_books.count
+
+    @realistic_fiction_books = RealisticFictionBook.all
+    @rfcount = @realistic_fiction_books.count
+
+    @science_fiction_books = ScienceFictionBook.all
+    @sfcount = @science_fiction_books.count
+
+    @traditional_literature_books = TraditionalLiteratureBook.all
+    @tlcount = @traditional_literature_books.count
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,9 @@ Rails.application.routes.draw do
   resources :historical_fiction_books
   resources :realistic_fiction_books
   resources :traditional_literature_books
-  #get 'home/index'
+  
   get 'home/about'
   get 'home/books'
-  #get 'traditional_literature_books/index'
-  #get 'realistic_ficiton_books/index'
-  #get 'historical_fiction_books/index'
-  #get 'fantasy_books/index'
-  #get 'science_fiction_books/index'
+  
   root 'home#index'
 end
